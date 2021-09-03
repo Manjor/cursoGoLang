47a4d26d4e0e54e5a9901c0280b33864695107bd
Manoel Tavares <manoeltavares.jr@gmail.com>
2021-09-03 08:58:55 -0300

    Comandos de Terminal / Constantes e Var

---
+++ fundamentos/comandos/comandos.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("Outro programa %s!")
+}
+
+// go - mostra alguns comandos da linguagel
+// go help - mostra comandos e conteudos de suporte
+// go version - mostra a versão do go
+// godoc -http=:6060 - disponibiliza a documentação offiline
+// go env - informações do ambiente
+// go env path - onde está o path principal do go
+// go env root - local de instalação do go
+// go doc cmd/vet - verifica códigos mortos
+// go build - gera um arquivo executável para o SO
+// go run - compila e executa o arquivo
