1124584383bea132fc8ecffba8659164562bdacd
Manoel Tavares <manoeltavares.jr@gmail.com>
2021-09-07 18:57:58 -0300

    For

---
+++ controles/for/for.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	i := 1
+	for i <= 10 { // não tem while em go
+		fmt.Println(i)
+		i++
+	}
+
+	for i := 0; i <= 20; i += 2 {
+		fmt.Printf("%d ", i)
+	}
+
+	fmt.Println("\nMisturando... ")
+
+	for i := 1; i <= 10; i++ {
+		if i%2 == 0 {
+			fmt.Println("Par")
+		} else {
+			fmt.Println("Impar")
+		}
+	}
+
+	// for { Loop infinito
+	// 	fmt.Println("Pra sempre")
+	// 	time.Sleep(time.Second) // Define um tempo de intervalo entre execuções
+	// }
+}
